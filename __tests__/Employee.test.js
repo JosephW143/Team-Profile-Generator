b208bbf2d27c6cc299fedf0390f11c62c756ff14
JosephW143 <josephwright1436@gmail.com>
2022-08-18 14:46:52 -0400

    created teset for employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require('../lib/Employee');
+
+test('checks for Employee methods', () => {
+    const employee = new employee ('Dave', '01', 'placeholder@email.com')
+
+    expect(employee.getRole()).toBe('Employee');
+    expect(employee.getName()).toBe('Dave');
+    expect(employee.getId()).toBe('01');
+    expect(employee.getEmail()).toBe('placeholder@email.com');
+});
