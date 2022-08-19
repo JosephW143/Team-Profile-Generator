2f4f3716d94bf55decd70ea5ed7a422bb5fb97c4
JosephW143 <josephwright1436@gmail.com>
2022-08-18 23:41:46 -0400

    create and pass manager test/all test pass

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,11 @@
+const Manager = require('../lib/Manager');
+
+test('checks for Manager methods', () => {
+    const manager = new Manager ('Dave', '01', 'placeholder@email.com', '(123)-456-7890')
+
+    expect(manager.getRole()).toBe('Manager');
+    expect(manager.getName()).toBe('Dave');
+    expect(manager.getId()).toBe('01');
+    expect(manager.getEmail()).toBe('placeholder@email.com');
+    expect(manager.officeNumber()).toBe('(123)-456-7890')
+});
