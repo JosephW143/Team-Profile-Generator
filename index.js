3caf3a28a9e523a39fcb489d0b104b35bd83ae38
JosephW143 <josephwright1436@gmail.com>
2022-08-20 14:29:02 -0400

    add dependencies in index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
